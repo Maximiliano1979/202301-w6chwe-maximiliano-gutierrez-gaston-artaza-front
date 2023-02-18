b887d8b02844eee59e1af1d594aa5c675fb91632
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-18 23:25:06 +0100

    Create custom hook

---
+++ src/hooks/useApi/useApi.ts
@@ -0,0 +1,18 @@
+import { useCallback } from "react";
+import { useAppDispatch } from "../../app/hooks";
+import { loadRobotsActionCreator } from "../../store/features/robots/robotsSlice";
+import { RobotApiResponse } from "../../types";
+
+const useApi = () => {
+  const dispatch = useAppDispatch();
+  const loadRobots = useCallback(async () => {
+    const result = await fetch("https://robots-4dix.onrender.com/robots");
+
+    const robots = (await result.json()) as RobotApiResponse;
+
+    dispatch(loadRobotsActionCreator(robots.robots));
+  }, [dispatch]);
+  return { loadRobots };
+};
+
+export default useApi;
